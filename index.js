ab1f05816029e9008496c0e621c20b5991a95ff0
charliepaull <charliepaull211@gmail.com>
2020-03-26 15:37:10 -0700

    first commit. cannot get mapping to work - does my access key work?

---
+++ index.js
@@ -0,0 +1,19 @@
+// let's create a map of the center of London w/ pretty Mapbox Street tiles
+
+// First we’ll initialize the map and set its view to our chosen geographical coordinates and a zoom level:
+let myMap = L.map("mapid").setView([51.505, -0.09], 13)
+
+// Note: setView also returns a map object - most Leaflet methods act like this when an explicit value is NOT returned
+
+// Add a tile layer to add to our map
+// Using a Mapbox tile layer here
+// have an access token as well
+// will have to adjust pixels later (want to use 256x256, not 512^2)
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiY2hhcmxpZXBhdWxsMjExIiwiYSI6ImNrODliY3NsejA1OGwza3BtbmV1dTZ1ZXMifQ.irXZ5CNwNM9EQ_YUcpJszg'
+}).addTo(mymap);
