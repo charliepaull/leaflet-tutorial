85ca8fbcf0b305c53337cd66376c51f6c537be13
charliepaull <charliepaull211@gmail.com>
2020-03-27 13:19:36 -0700

    went through the tutorial on leaflet website. learned popups, click events, polygons, and circles. access key still in first commit but it does not matter.

--- index.js
+++ index.js
@@ -19,4 +19,66 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: mykey
-}).addTo(mymap);
+}).addTo(myMap);
+
+// add a marker to the map (can make this a user's current location but will just be at specific coordinates for the moment)
+let marker = L.marker([51.5, -0.09]).addTo(myMap);
+
+// adding a circle is the exact same but allows for a 2nd argument to specify the radius in meters
+// can also specify color, opacity, and fill color
+// let circle = L.circle(coords (coords are in an arr[]), circle attr.)
+// DONT FORGET .addTo(name of map)
+let circle = L.circle([51.508, -0.11], {
+    color: "blue",
+    fillColor: "#3399FF",
+    opacity: 0.35,
+    radius: 500
+}).addTo(myMap)
+
+// to add a polygon, just add the coordinates wanted (to as many sides as needed)
+// polygon coords is an array of arrs [[]]
+let polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(myMap)
+
+// working with popups
+// used to attach some information to a particular object
+marker.bindPopup("<b>This is</b><br>your current location").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+// Can also use popups as layers (when you need something more than attaching a popup to an object):
+// a popup that isn't attached to an object - just a general spot, or maybe where someone clicks on the map
+
+// use openOn instead of addTo because it handles automatic closing of a previously opened popup when opening a new one which is good for usability.
+// let popup = L.popup()
+//     .setLatLng([51.5, -0.09])
+//     .setContent("I am a standalone popup.")
+//     .openOn(myMap);
+
+
+// Dealing with Events
+// every event in Leaflet (such as a user click), the corresponding object sends an event where you can add a function to it so that something happens - allows for a reaction to a user interaction
+
+// first argument: listener function - an event object
+
+// ex of an alert showing up if clicked anywhere on map, will show coordinates of clicked spot
+// function onMapClick(e) {
+//     alert("You clicked the map at " + e.latlng);
+// }
+
+// myMap.on('click', onMapClick);
+
+// makes this a popup instead of an alert
+let popup = L.popup();
+
+function onMapClick(event) {
+    popup
+        .setLatLng(event.latlng)
+        .setContent("You clicked the map at " + event.latlng.toString())
+        .openOn(myMap);
+}
+
+myMap.on('click', onMapClick);
